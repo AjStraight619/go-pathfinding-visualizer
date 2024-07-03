8a17ad7a6024f182568177121a12282e446dd004
Alex Straight <alex@Alexs-Laptop.local>
2024-07-03 14:46:25 -0700

    A star is computing and generating correct path

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/AJStraight619/go-pathfinding-visualizer/internal/algorithms"
 	"github.com/AJStraight619/go-pathfinding-visualizer/internal/pathfinding"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 func main() {
 	p := pathfinding.InitPathfinding()
+	p.SetAlgorithm(algorithms.AStar)
 
 	ebiten.SetWindowSize(1200, 900)
 	ebiten.SetWindowTitle("Pathfinding Visualizer")
